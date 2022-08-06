6dbc73465e649301250565e9a89b21ffcb7a5c91
SaishoAlita <xxdragninjaxx@gmail.com>
2022-08-06 22:36:56 +0300

    added extra api calls and database implementations

--- Wave/Controllers/ApiController.cs
+++ Wave/Controllers/ApiController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -43,21 +44,81 @@ namespace Wave.Controllers
                 {
                     // register phone here and create all the stuff uknow 
                     // incomplete it does absolutely nothing rn
+                    Customer customer;
                     if (!Common.CheckIfPhoneNumberAlreadyRegistered(phoneNumber)) 
                     {
-                        Common.AddNewCustomer(phoneNumber);
+                        customer = Common.AddNewCustomer(phoneNumber);
                         FormsAuthentication.SetAuthCookie(phoneNumber, true);
                     }
                     else
                     {
+                        WaveEntities db = new WaveEntities();
+                        customer = db.Customers.Where(e => e.PhoneNumber == phoneNumber).FirstOrDefault();
                         FormsAuthentication.SetAuthCookie(phoneNumber, true);
                     }
-                    return Json(Common.BuildGeneralResponseJson(true, ResponseCode.OTPVerificationSuccessful, "Phone number verified!", "http://localhost/Home/Index"));
+                    return Json(Common.BuildGeneralResponseJson(true, ResponseCode.OTPVerificationSuccessful, "Phone number verified!", "http://localhost/User?guid=" + customer.LoyaltyCardGUID));
                 }
                 Session["OTPSubmitTries"] = Session["OTPSubmitTries"] != null ? (int)Session["OTPSubmitTries"] + 1 : 1;
                 return Json(Common.BuildGeneralResponseJson(false, ResponseCode.IncorrectOTP, "Incorrect OTP!"));
             }
             return Json(Common.BuildGeneralResponseJson(false, ResponseCode.ExceededMaximumOTPSubmissions, "Exceeded maximum tries, please try again in 1 hour."));
+        }
+
+        [HttpPost]
+        public JsonResult RetrieveCustomerInformation(string loyaltyCardGUID)
+        {
+            WaveEntities db = new WaveEntities();
+            Customer customer = db.Customers.Where(e => e.LoyaltyCardGUID == loyaltyCardGUID).FirstOrDefault();
+            if(customer != null)
+            {
+                return Json(Common.BuildDataResponseJson(true, ResponseCode.RequestFulfilled, "Successfully retrieved data.", JsonConvert.SerializeObject(customer)));
+            }
+
+            else
+            {
+                return Json(Common.BuildGeneralResponseJson(false, ResponseCode.UnknownError, "Customer does not exist!"));
+            }
+        }
+
+        [HttpPost]
+        public JsonResult ModifyLoyaltyPoints(string loyaltyCardGUID, int newValue)
+        {
+            if(newValue <= 6)
+            {
+                WaveEntities db = new WaveEntities();
+                Customer customer = db.Customers.Where(e => e.LoyaltyCardGUID == loyaltyCardGUID).FirstOrDefault();
+                customer.LoyaltyPoints = newValue;
+                customer.Scans.Add(new Scan() { ScanDate = DateTime.Now });
+                db.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return Json(Common.BuildGeneralResponseJson(true, ResponseCode.LoyaltyPointsModified, "Successfully modified loyalty points."));
+            }
+            else
+            {
+                return Json(Common.BuildGeneralResponseJson(false, ResponseCode.UnknownError, "New value is greater than the maximum amount allowed."));
+            }
+        }
+
+        public JsonResult BanUser(string loyaltyCardGUID)
+        {
+            WaveEntities db = new WaveEntities();
+            Customer customer = db.Customers.Where(e => e.LoyaltyCardGUID == loyaltyCardGUID).FirstOrDefault();
+            customer.IsActive = false;
+            customer.IsBanned = true;
+            db.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return Json(Common.BuildGeneralResponseJson(true, ResponseCode.Banned, "Successfully banned user."));
+        }
+
+        public JsonResult UnbanUser(string loyaltyCardGUID)
+        {
+            WaveEntities db = new WaveEntities();
+            Customer customer = db.Customers.Where(e => e.LoyaltyCardGUID == loyaltyCardGUID).FirstOrDefault();
+            customer.IsActive = true;
+            customer.IsBanned = false;
+            db.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return Json(Common.BuildGeneralResponseJson(true, ResponseCode.RequestFulfilled, "Successfully unbanned user."));
         }
 
         public class Common
@@ -66,12 +127,17 @@ namespace Wave.Controllers
             private static string infobipUrl = "https://mp62k2.api.infobip.com";
             public static object BuildGeneralResponseJson(bool isSuccess, ResponseCode responseCode, string message)
             {
-                return new { success = isSuccess, code = (int)responseCode, message = message };
+                return new { success = isSuccess, code = (int)responseCode, message = message, action = Action.Passive };
             }
 
             public static object BuildGeneralResponseJson(bool isSuccess, ResponseCode responseCode, string message, string redirectUri)
             {
-                return new { success = isSuccess, code = (int)responseCode, message = message, redirectUri = redirectUri };
+                return new { success = isSuccess, code = (int)responseCode, message = message, action = Action.Redirect, redirectUri = redirectUri };
+            }
+
+            public static object BuildDataResponseJson(bool isSuccess, ResponseCode responseCode, string message, string data)
+            {
+                return new { success = isSuccess, code = (int)responseCode, message, action = Action.Passive, data };
             }
 
 
@@ -120,7 +186,7 @@ namespace Wave.Controllers
                 }
             }
 
-            public static void AddNewCustomer(string phoneNumber)
+            public static Customer AddNewCustomer(string phoneNumber)
             {
                 WaveEntities db = new WaveEntities();
                 Customer customer = new Customer()
@@ -132,6 +198,8 @@ namespace Wave.Controllers
                     LoyaltyCardGUID = GenerateShortGuid(),
                 };
                 db.Entry(customer).State = System.Data.Entity.EntityState.Added;
+                db.SaveChanges();
+                return customer;
             }
 
             public static string GenerateShortGuid()
@@ -149,6 +217,8 @@ namespace Wave.Controllers
             OTPRequestSuccessful = 10,
             OTPVerificationSuccessful = 11,
 
+            RequestFulfilled = 12,
+
             // Loyalty concerns
             LoyaltyPointAdded = 15,
             LoyaltyPointsModified = 16,
@@ -165,6 +235,12 @@ namespace Wave.Controllers
 
             // Informational
             Information = 45
+        }
+
+        public enum Action
+        {
+            Passive = 0,
+            Redirect = 1
         }
 
     }
